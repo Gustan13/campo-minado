62a39b58875f6a2b5725dd32479d659b1cd65bc7
Gustavo Isoo Binder <gustan.binder@gmail.com>
2022-12-16 20:09:56 -0300

    add: funcao que calcula bombas ao redor.

--- lib.c
+++ lib.c
@@ -38,12 +38,44 @@ int checa_limites(int row, int col)
     return 1;
 }
 
+void calcula_bloco(int *m, int row, int col)
+{
+    int num_bombas = 0;
+
+    if (*(m + row * MAX + col) == 9)
+        return;
+
+    for (int i = row - 1; i <= row + 1; i++)
+    {
+        for (int j = col - 1; j <= col + 1; j++)
+        {
+            if (checa_limites(i, j) < 1)
+                continue;
+
+            if (*(m + i * MAX + j) == 9)
+                num_bombas++;
+        }
+    }
+
+    *(m + row * MAX + col) = num_bombas;
+}
+
+void calcula_blocos(int *m)
+{
+    for (int i = 0; i < MAX; i++)
+    {
+        for (int j = 0; j < MAX; j++)
+            calcula_bloco(m, i, j);
+    }
+}
+
 int main()
 {
     int *p_matriz = cria_matriz();
 
     system("clear");
     insere_bombas(p_matriz);
+    calcula_blocos(p_matriz);
     imprime_matriz(p_matriz);
     return 0;
 }
