080a1a8fab0f05f9bf2ea51c695709fe37d074e4
Gustavo Isoo Binder <gustan.binder@gmail.com>
2022-12-16 19:52:14 -0300

    add: funcao que imprime matriz.

--- lib.c
+++ lib.c
@@ -14,13 +14,29 @@ void insere_bombas(int *m)
     {
         for (int j = 0; j < MAX; j++)
         {
-            *(m + i * MAX + j) = rand() % 2 == 0? (-1) : (0);
+            *(m + i * MAX + j) = rand() % 3 == 0? (9) : (0);
         }
     }
+}
+
+void imprime_matriz(int *m)
+{
+    for (int i = 0; i < MAX; i++)
+    {
+        for (int j = 0; j < MAX; j++)
+        {
+            printf("%d ", *(m + i * MAX + j));
+        }
+        printf("\n");
+    } 
 }
 
 int main()
 {
     int *p_matriz = cria_matriz();
+
+    system("clear");
+    insere_bombas(p_matriz);
+    imprime_matriz(p_matriz);
     return 0;
 }
