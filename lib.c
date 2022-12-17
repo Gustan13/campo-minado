d91ec4d127b4704a84e43dc8d4ea45a65f8bd997
Gustavo Isoo Binder <gustan.binder@gmail.com>
2022-12-16 21:19:35 -0300

    add: funcao que libera bloco.

--- lib.c
+++ lib.c
@@ -14,7 +14,7 @@ void insere_bombas(int *m)
     {
         for (int j = 0; j < MAX; j++)
         {
-            *(m + i * MAX + j) = rand() % 3 == 0? (9) : (0);
+            *(m + i * MAX + j) = rand() % 2 == 0? (9) : (0);
         }
     }
 }
@@ -34,7 +34,7 @@ void imprime_matriz(int *m)
 
 int checa_limites(int row, int col)
 {
-    if (row < 0 || row > MAX || col < 0 || col > MAX)
+    if (row < 0 || row > MAX - 1|| col < 0 || col > MAX - 1)
         return 0;
     return 1;
 }
@@ -75,6 +75,17 @@ void recebe_comando(int *row, int *col)
     scanf("%d %d", row, col);
 }
 
+void libera_bloco(int *m, int *m_h, int row, int col)
+{
+    if (checa_limites(row, col) < 1)
+        return;
+
+    *(m_h + row * MAX + col) = *(m + row * MAX + col);
+
+    if (*(m + row * MAX + col) != 0)
+        return; 
+}
+
 int main()
 {
     int *p_matriz = cria_matriz();
@@ -82,15 +93,29 @@ int main()
     int row, col;
     int running = 1;
 
-    system("clear");
     insere_bombas(p_matriz);
     calcula_blocos(p_matriz);
 
-    recebe_comando(&row, &col);
-    if (checa_limites(row, col))
+    system("clear");
+    printf("Campo Minado do Binder\n9 significa bomba.\n\n");
+    imprime_matriz(p_matriz_h);
+
+    while (running)
     {
+        recebe_comando(&row, &col);
+        if (!checa_limites(row, col))
+            continue;
+        
+        if (*(p_matriz + row * MAX + col) == 9)
+            running--;
+
+        libera_bloco(p_matriz, p_matriz_h, row, col);    
+        system("clear");
+        printf("Campo Minado do Binder\n9 significa bomba.\n\n");
         imprime_matriz(p_matriz_h);
     }
+
+    printf("\nBOOM! Perdeste.\n\n");
     
     return 0;
 }
