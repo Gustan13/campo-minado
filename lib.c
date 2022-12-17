e50d2af9cf2d6cf5af953026b1c1f9a3286d1911
Gustavo Isoo Binder <gustan.binder@gmail.com>
2022-12-17 02:02:53 -0300

    lib.c

--- lib.c
+++ lib.c
@@ -1,22 +1,47 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define MAX 5
+#define MAX 10
 
 int *cria_matriz()
 {
     return malloc(sizeof(int) * MAX * MAX);
 }
 
-void insere_bombas(int *m)
+void preenche_matriz_h(int *m)
 {
     for (int i = 0; i < MAX; i++)
     {
         for (int j = 0; j < MAX; j++)
         {
-            *(m + i * MAX + j) = rand() % 2 == 0? (9) : (0);
+            *(m + i * MAX + j) = 9;
         }
     }
+}
+
+void insere_bombas(int *m, int bombas)
+{
+    int num = 0;
+
+    for (int i = 0; i < MAX; i++)
+    {
+        for (int j = 0; j < MAX; j++)
+        {
+            if (rand() % 3 == 0)
+            {
+                *(m + i * MAX + j) = 0;
+                if (num < bombas)
+                {
+                    num++;
+                    *(m + i * MAX + j) = -1;
+                }
+            } else
+                *(m + i * MAX + j) = 0;
+        }
+    }
+
+    if (num < bombas)
+        insere_bombas(m, bombas);
 }
 
 void imprime_matriz(int *m)
@@ -29,7 +54,6 @@ void imprime_matriz(int *m)
         }
         printf("\n");
     }
-    printf("\n");
 }
 
 int checa_limites(int row, int col)
@@ -43,7 +67,7 @@ void calcula_bloco(int *m, int row, int col)
 {
     int num_bombas = 0;
 
-    if (*(m + row * MAX + col) == 9)
+    if (*(m + row * MAX + col) == -1)
         return;
 
     for (int i = row - 1; i <= row + 1; i++)
@@ -53,7 +77,7 @@ void calcula_bloco(int *m, int row, int col)
             if (checa_limites(i, j) < 1)
                 continue;
 
-            if (*(m + i * MAX + j) == 9)
+            if (*(m + i * MAX + j) == -1)
                 num_bombas++;
         }
     }
@@ -72,18 +96,49 @@ void calcula_blocos(int *m)
 
 void recebe_comando(int *row, int *col)
 {
-    scanf("%d %d", row, col);
+    int i = 0, j = 0;
+    scanf("%d %d", &i, &j);
+    *row = i - 1;
+    *col = j - 1;
 }
 
-void libera_bloco(int *m, int *m_h, int row, int col)
+void libera_bloco(int* m, int* m_h, int row, int col)
 {
     if (checa_limites(row, col) < 1)
         return;
 
-    *(m_h + row * MAX + col) = *(m + row * MAX + col);
-
     if (*(m + row * MAX + col) != 0)
-        return; 
+    {
+        *(m_h + row * MAX + col) = *(m + row * MAX + col);
+        return;
+    }
+
+    if (*(m_h + row * MAX + col) == 9)
+    {
+        *(m_h + row * MAX + col) = *(m + row * MAX + col);
+
+        for (int i = - 1; i <= 1; i++)
+        {
+            for (int j = -1; j <= 1; j++)
+                libera_bloco(m, m_h, row + i, col + j);
+        }
+    }
+}
+
+int numero_elemntos(int *m, int elemento)
+{
+    int n = 0;
+
+    for (int i = 0; i < MAX; i++)
+    {
+        for (int j = 0; j < MAX; j++)
+        {
+            if (*(m + i * MAX + j) == elemento)
+                n++;
+        }
+    }
+    
+    return n;
 }
 
 int main()
@@ -91,13 +146,14 @@ int main()
     int *p_matriz = cria_matriz();
     int *p_matriz_h = cria_matriz();
     int row, col;
-    int running = 1;
+    int running = 1, bombas = 10;
 
-    insere_bombas(p_matriz);
+    preenche_matriz_h(p_matriz_h);
+    insere_bombas(p_matriz, bombas);
     calcula_blocos(p_matriz);
 
-    system("clear");
-    printf("Campo Minado do Binder\n9 significa bomba.\n\n");
+    system("cls");
+    printf("Campo Minado do Binder\n\n");
     imprime_matriz(p_matriz_h);
 
     while (running)
@@ -106,16 +162,30 @@ int main()
         if (!checa_limites(row, col))
             continue;
         
-        if (*(p_matriz + row * MAX + col) == 9)
+        if (*(p_matriz + row * MAX + col) == -1)
             running--;
 
         libera_bloco(p_matriz, p_matriz_h, row, col);    
-        system("clear");
-        printf("Campo Minado do Binder\n9 significa bomba.\n\n");
+
+        system("cls");
+        printf("Campo Minado do Binder\n\n");
         imprime_matriz(p_matriz_h);
+
+        if (numero_elemntos(p_matriz_h, 9) == bombas)
+            running = 0;
     }
 
-    printf("\nBOOM! Perdeste.\n\n");
+    if (numero_elemntos(p_matriz_h, 9) == bombas)
+    {
+        printf("\nGANHASTE. CONFETTIS!!!\n\n");
+    }
+    else
+        printf("\nBOOM! Perdeste.\n\n");
+
+    free(p_matriz);
+    free(p_matriz_h);
+    p_matriz = NULL;
+    p_matriz_h = NULL;
     
     return 0;
 }
